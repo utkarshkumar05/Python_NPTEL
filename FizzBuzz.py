838cf4b8e2c2c6a14952f74bcbb3737595d30b2e
DrParadox05 <rksingh.rks6661@gmail.com>
2021-03-04 22:38:30 +0530

    Complete FIzzBuzz (including user input)

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,17 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar  4 19:41:48 2021
-
-@author: utkarsh kumar
-"""
-
-
-for i in range(1,51):
-    if(i%3==0):
-        print(str(i)+" = Fizz")
-    elif(i%5==0):
-        print(str(i)+" = Buzz")
+def FizzBuzz(n):
+    for i  in range(1, n+1):
         if(i%3==0 and i%5==0):
-            print(str(i)+" = FizzBuzz")
-    else:
-        print(str(i))
+            print(str(i)+"= FizzBuzz")
+        else:
+            if(i%3==0):
+                print(str(i)+"= Fizz")
+            else:
+                if(i%5==0):
+                    print(str(i)+"= Buzz")
+                else:
+                    print(str(i))
+                    
+FizzBuzz(51)
+            
