1a41f2c5607ba0e9d1010dddfb5dd09b213687ec
DrParadox05 <rksingh.rks6661@gmail.com>
2021-03-11 12:25:54 +0530

    Complete Jumbled words game(Upto, Question creation by the computer)

--- Jumbled_Words.py
+++ Jumbled_Words.py
@@ -1,3 +1,27 @@
+import random
+
+def choose():
+    words = ['rainbow', 'computer', 'science', 'programming', 'mathematics', 'player', 'condition', 'reverse', 'board', 'water']
+    pick = random.choice(words)
+    return pick
+
+def jumble(word):
+    jumbled = "".join(random.sample(word, len(word)))
+    return jumbled
+
+def thank(p1n,p2n,p1,p2):
+    p1=int(p1)
+    p2=int(p2)
+    if(p1>p2):
+        print('The winner is ',p1n)
+    else:
+        if(p2>p1):
+            print('The winner is ',p2n)
+        else:
+            print('You both played well, but it is a tie')
+    print(p1n,' : ',p1)
+    print(p2n,' : ',p2)
+
 def play():
     p1name = input('Name of player1 = ')
     p2name = input('Name of player2 = ')
@@ -9,6 +33,7 @@ def play():
         picked_word = choose()
         #Creating Question
         qn = jumble(picked_word)
+        print(qn)
         #Process of Game
         #For Player 1
         if(turns%2==0):
@@ -38,4 +63,5 @@ def play():
             if(c==0):
                 thank(p1name,p2name,pp1,pp2)
                 break
-        
+        turns = turns+1
+play()
